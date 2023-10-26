6cf607cbe87c283464201256af96d670d8f3506c
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-26 09:33:16 +0600

    get full api & get api id wise

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 app.use(cors());
@@ -23,6 +23,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const serviceCollection = client.db('carDoctor').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                // Include only the `title` and `imdb` fields in each returned document
+                projection: { _id: 1, title: 1, price: 1, service_id: 1 },
+            };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
