8fd8faad368f503e02d1eaf48a4995f3e7245caf
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-28 12:56:50 +0600

    JWT added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
-app.use(cors());
+// send token to client side
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjwj9uc.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,13 +28,58 @@ const client = new MongoClient(uri, {
     }
 });
 
+// My created middleware
+const logger = async (req, res, next) => {
+    console.log('called:', req.host, req.originalUrl)
+    next();
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+
+    console.log('value of token in middleware', token);
+
+    if (!token) {
+        return res.status(401).send({ message: 'not authorized' })
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        // error
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        // if token is valid then it would be decoded
+        console.log('value in the token', decoded);
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('carDoctor').collection('services');
 
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
-        app.get('/services', async (req, res) => {
+
+        // auth related api
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            // create token
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '12h' })
+
+            // set to cookie
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false
+                })
+                .send({ success: true });
+        })
+
+
+        app.get('/services', logger, async (req, res) => {
             const cursor = serviceCollection.find({});
             const result = await cursor.toArray();
             res.send(result);
@@ -47,12 +99,36 @@ async function run() {
         })
 
         // get booking filtering by email
-        app.get('/bookings', async (req, res) => {
-            const { email } = req.query;
-            const query = { email };
+        // app.get('/bookings', logger, verifyToken, async (req, res) => {
+        //     // console.log('received token', req.cookies.token);
+        //     console.log('user in the valid token', req.user);
+
+        //     if (req.query.email !== req.user.email) {
+        //         return res.status(403).send({ message: 'forbidden access' })
+        //     }
+
+        //     const { email } = req.query;
+        //     const query = { email };
+        //     const result = await bookingCollection.find(query).toArray();
+        //     res.send(result);
+        // });
+
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            // console.log('ttttt token', req.cookies.token)
+            console.log('user in the valid token', req.user)
+
+            if (req.query.email !== req.user.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
-        });
+        })
 
         // app.get('/bookings/:id', async (req, res) => {
         //     const id = req.params.id;
